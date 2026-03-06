20ceeafaef17bf0090bd7d1aea928678f04aac92
Andreas Burger <andreas.burger@mail.utoronto.ca>
2026-03-06 10:56:00 -0500

    fixed: ModuleNotFoundError: No module named 'scripts'

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -1,11 +1,12 @@
 ## Wrapper for mace.cli.run_train.main ##
 
 from mace.cli.run_train import train_main
-from scripts.eval_horm import evaluate_hessian_on_horm_dataset
-
+    
 if __name__ == "__main__":
     args = train_main()
     
+    from eval_horm import evaluate_hessian_on_horm_dataset
+
     df_results, aggregated_results = evaluate_hessian_on_horm_dataset(
         args=args,
         max_samples=1000,
